0ddc4994e12ca8fc350ff6a257b2e2c5355b19f1
andrew-lukyanov <nefesh.samael@gmail.com>
2021-05-20 02:00:59 +0300

    Add components to module

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserAPI } from '../../api';
-import { Toastr } from "../../providers"
-import { Auth } from "../../providers"
+import { UserAPI } from '../api';
+import { Toast } from "../providers"
+import { Auth } from "../providers"
 
 @Component({
   selector: 'app-sign-up',
@@ -18,16 +18,16 @@ export class SignUpComponent {
     password_confirmation: new FormControl(null, Validators.required)
   })
 
-  constructor(private userAPI: UserAPI, private toastr: Toastr, private router: Router, private auth: Auth) { }
+  constructor(private userAPI: UserAPI, private toast: Toast, private router: Router, private auth: Auth) { }
 
   createUser() {
     this.userAPI.create({user: this.userForm.value}).subscribe((data) => {
-      this.toastr.success("Registered");
+      // this.toast.success("Registered");
       this.auth.updateUser(data);
       this.router.navigate(['/playlists']);
     }, (error) => {
       console.log('error', error);
-      this.toastr.showResponseErrors(error)
+      // this.toast.showResponseErrors(error)
     })
   }
 }
